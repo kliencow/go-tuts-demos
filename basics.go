966720a1d1dd225311cbe7226b6ee910ee594e73
Walt Norblad <walt.norblad@lunarlogic.com>
2014-04-23 20:30:19 -0700

    some cleanup and split up website examples into two files.

--- basics.go
+++ basics.go
@@ -1,99 +1,86 @@
-package main 
+package main
 
-/*
-
-    http://golang.org/doc/effective_go.html
-
-
-*/
 import (
-    "fmt"
+	"fmt"
 )
 
-
 func arrayPlay() {
-    fmt.Println("ARRS!")
-    arr := [3]int{4,5,6}
+	fmt.Println("ARRS!")
+	arr := [3]int{4, 5, 6}
 
-    for i,v := range arr {
-        fmt.Println(i, ": ", v)
-    }    
+	for i, v := range arr {
+		fmt.Println(i, ": ", v)
+	}
 
 }
-
 
 func niceSlice() {
-    fmt.Println("SLICES!")
-    slice := []int{5,6,7,8,9}
+	fmt.Println("SLICES!")
+	slice := []int{5, 6, 7, 8, 9}
 
-    fmt.Println("Slice Length: ", len(slice))
-    fmt.Println("Slice Capacity: ", cap(slice))
+	fmt.Println("Slice Length: ", len(slice))
+	fmt.Println("Slice Capacity: ", cap(slice))
 
-    fmt.Println(slice[3:5])
+	fmt.Println(slice[3:5])
 }
-
 
 func meanSlice() {
-    fmt.Println("MORE SLICES!")
+	fmt.Println("MORE SLICES!")
 
-    slice := make([]int, 2, 4)
-    fmt.Println("Slice Length: ", len(slice))
-    fmt.Println("Slice Capacity: ", cap(slice))
+	slice := make([]int, 2, 4)
+	fmt.Println("Slice Length: ", len(slice))
+	fmt.Println("Slice Capacity: ", cap(slice))
 
-    fmt.Println(slice)
+	fmt.Println(slice)
 
-    slice[1] = 2
-    fmt.Println(slice)
+	slice[1] = 2
+	fmt.Println(slice)
 
-    slice = slice[:4]
-    fmt.Println(slice)
+	slice = slice[:4]
+	fmt.Println(slice)
 
 }
 
+func sliceMadeInteresting() {
+	fmt.Println("MORE SLICES GO!")
 
-func sliceMadeInteresting(){
-    fmt.Println("MORE SLICES GO!")
+	s := []int{1, 2, 3, 4, 5, 6, 7}
+	s2 := s[2:4]
+	s3 := dontCare(s)
 
-    s := []int{1,2,3,4,5,6,7}
-    s2 := s[2:4]
-    s3 := dontCare(s)
-    
-    fmt.Println(&s[2])    
-    fmt.Println(&s2[0])
-    fmt.Println(&s3[0])
+	fmt.Println(&s[2])
+	fmt.Println(&s2[0])
+	fmt.Println(&s3[0])
 }
 
 func dontCare(s []int) (interesting []int) {
-    bit := s[2:4]
+	bit := s[2:4]
 
-    interesting = make([]int, len(bit))
+	interesting = make([]int, len(bit))
 
-    copy(interesting, bit)
-    return
+	copy(interesting, bit)
+	return
 }
-
 
 func mapsMadeFun() {
-    fmt.Println("MAPS!")
-    m := make(map[string]int)
-    m["Foo"] = 3
-    m["Bar"] = 4
-    fmt.Println(m)
+	fmt.Println("MAPS!")
+	m := make(map[string]int)
+	m["Foo"] = 3
+	m["Bar"] = 4
+	fmt.Println(m)
 
-    for k, v := range m {
-        fmt.Println(k, ": ", v)
-    }
+	for k, v := range m {
+		fmt.Println(k, ": ", v)
+	}
 }
 
-
 func main() {
-    //arrayPlay()
-    //niceSlice()
-    //meanSlice()
+	//arrayPlay()
+	//niceSlice()
+	//meanSlice()
 
-    //sliceMadeInteresting()
-    
-    mapsMadeFun()
+	//sliceMadeInteresting()
 
+	mapsMadeFun()
 
 }
--- simple-website.go
+++ simple-website.go
@@ -3,24 +3,24 @@ package main
 /*
    This simple website shows different system for creating basic webpages.
 
+    This is getting out of hand. I need to break this out in someway to make it managable.
+
 */
 
 import (
 	"fmt"
 	"github.com/gorilla/mux"
-	//"html/template"
 	"net/http"
+	"os"
 	"text/template"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/", Home)
 	r.HandleFunc("/simplestTemplate", simplestTemplate)
 	r.HandleFunc("/simpleInlineTemplate", simpleInlineTemplate)
-	r.HandleFunc("/simpleImportedTemplate", simpleImportedTemplate)
-	r.HandleFunc("/complexImportedTemplate", complexImportedTemplate)
-	r.HandleFunc("/fullyTemplatedTemplate", fullyTemplatedTemplate)
+
 	http.Handle("/", r)
 	http.ListenAndServe(":9999", nil)
 }
@@ -28,29 +28,36 @@ func main() {
 /*
    This simple home handler show how to imput a parameter as a single option which is interpreted as %s for string.
 */
-func HomeHandler(response http.ResponseWriter, request *http.Request) {
+func Home(response http.ResponseWriter, request *http.Request) {
 	page := `
 <html>
     <head></head>
     <body>
         <h1>Index of examples</h1>
+        <h2>Notes</h2>
+        <div>
+            %s
+        </div>
         <ul>
             <li><a href="/simplestTemplate">Simplest Template</a> -- a template with variables subbed in via Fprintf.</li>
-            <li><a href="/simpleInlineTemplate">Simple Inline Template</a> -- a template with multiple variables and a loop./li>
-            <li><a href="/simpleImportedTemplate">Simple Imported Template</a> -- a template imported from an HTML file.</li>
-            <li><a href="/complexImportedTemplate">Complex Imported Template</a> -- a template imported from an HTML file with lots of variables.</li>
-            <li><a href="/fullyTemplatedTemplate">Fully Templated Template</a> -- a template that incorporates loops, other template files, variables, and loops. Also includes css, images, and js files.</li>            
+            <li><a href="/simpleInlineTemplate">Simple Inline Template</a> -- a template with multiple variables and a loop.</li>            
         </ul>
     </body>
 </html>`
-	fmt.Fprintf(response, page)
-}
 
-/**
- *
- *  EXAMPLE 1: SIMPLEST TEMPLATE
- *
- */
+	f, err := os.Open(".")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	fileInfo, _ := f.Stat()
+
+	testString := fileInfo.IsDir()
+
+	fmt.Fprintf(response, page, testString)
+}
 
 /*
    This example shows how to output using fmt.Fprintf. The trick going on here is we can pass as many variables to Fprintf as we'd like. The
@@ -76,12 +83,6 @@ func simplestTemplate(response http.ResponseWriter, request *http.Request) {
 }
 
 /*
- *
- *  EXAMPLE 2: SIMPLE INLINE TEMPLATE
- *
- */
-
-/*
    Example struct for simpleInlineTemplate
 */
 type PageDeets struct {
@@ -102,6 +103,7 @@ func simpleInlineTemplate(response http.ResponseWriter, request *http.Request) {
 		AnotherTitle: "Another TEST page",
 		OneHundred:   100,
 	}
+
 	page := `
 <html>
     <head></head>
@@ -111,6 +113,7 @@ func simpleInlineTemplate(response http.ResponseWriter, request *http.Request) {
         <h3>The number: {{.OneHundred}}</h3>
     </body>
 </html>`
+
 	t, err := template.New("Test").Parse(page)
 	if err != nil {
 		fmt.Println(err)
@@ -120,17 +123,4 @@ func simpleInlineTemplate(response http.ResponseWriter, request *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-
-}
-
-func simpleImportedTemplate(response http.ResponseWriter, request *http.Request) {
-
-}
-
-func complexImportedTemplate(response http.ResponseWriter, request *http.Request) {
-
-}
-
-func fullyTemplatedTemplate(response http.ResponseWriter, request *http.Request) {
-
 }
